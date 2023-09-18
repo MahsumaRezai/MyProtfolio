10a98073d098b719027928b04c723206f3533dff
Masoumeh Rezai <you@example.com>
2023-09-19 04:29:36 +0430

    add section one of project

--- src/Pages/Works/Project.js
+++ src/Pages/Works/Project.js
@@ -1,8 +1,12 @@
 import { Fragment } from 'react';
 import classes from './Work.module.css';
 import Card from './Card';
-import img1 from './image/imgone.PNG';
 import React from 'react';
+import img1 from './image/imgone.PNG';
+import img2 from './image/imgTwo.png';
+import img3 from './image/food.png';
+
+
 
 
 const Project = (props) => {
@@ -12,6 +16,24 @@ const Project = (props) => {
                 <Card>
                     <div className={classes.warrper}>
                         <img src={img1} alt="" className={classes.imgone} />
+                    </div>
+                    <div className={classes.button}>
+                        <button className={classes.btn}>Read More</button>
+                    </div>
+
+                </Card>
+                <Card>
+                    <div className={classes.warrper}>
+                        <img src={img2} alt="" className={classes.imgtwo} />
+                    </div>
+                    <div className={classes.button}>
+                        <button className={classes.btn}>Read More</button>
+                    </div>
+
+                </Card>
+                <Card>
+                    <div className={classes.warrper}>
+                        <img src={img3} alt="" className={classes.imgtwo} />
                     </div>
                     <div className={classes.button}>
                         <button className={classes.btn}>Read More</button>
