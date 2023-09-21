13caeb82461c46e3f90c02239d7560731919fdc1
Masoumeh Rezai <you@example.com>
2023-09-22 01:25:14 +0430

    add last commit for today

--- src/Pages/Contact/Contacts.js
+++ src/Pages/Contact/Contacts.js
@@ -1,23 +1,43 @@
 import { Fragment } from "react";
 import React from 'react';
 import classes from './Contact.module.css'
-import Cart from "./Cart";
+import img1 from './image/github.png';
+import img2 from './image/twitter.png';
+import img3 from './image/gmail.png';
+import img4 from './image/link.png';
+
+
 
 
 const Contacts = (props) => {
     return (
         <Fragment>
             <div className={classes.contact} >
-                <Cart>
+                <div className={classes.warrp}>
+                    <h2 className={classes.maintitle}>Get in Touch</h2>
                     <div>
-                        <div>
-                            <p>If you want to follow me, you can follow me from the following networks</p>
-                        </div>
-                        <div>
-
-                        </div>
+                        <p className={classes.title}>If you want to follow me, you can follow me from the following networks</p>
                     </div>
-                </Cart>
+                    <div className={classes.image}>
+                        <a href="https://github.com/MahsumaRezai">
+                            <img src={img1} alt="" className={classes.img} />
+                        </a>
+                        <a href='https://twitter.com/MahsumaRezai'>
+                            <img src={img2} alt="" className={classes.img} />
+                        </a>
+                        <a href="mahsuma.rezai8@gmail.com">
+                            <img src={img3} alt="" className={classes.img} />
+
+                        </a>
+                        <a href="https://www.linkedin.com/in/masoumeh-rezai-99826a262/">
+                            <img src={img4} alt="" className={classes.img} />
+
+                        </a>
+
+
+
+                    </div>
+                </div>
 
 
             </div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,15 @@
 import { Fragment } from "react";
 import About from "./About";
 import Work from "./Work";
-import React  from 'react';
+import React from 'react';
+import Contact from "./Contact";
 
 const Home = (props) => {
     return (
         <Fragment>
             <About />
             <Work />
+            <Contact />
         </Fragment>
     )
 }
