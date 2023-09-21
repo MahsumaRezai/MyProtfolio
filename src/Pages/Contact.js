38b4c983b065c37911d658a040ece3be630d0641
Masoumeh Rezai <you@example.com>
2023-09-21 23:02:17 +0430

    add conatct au

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,11 +1,14 @@
 import { Fragment } from "react";
-import React  from 'react'
+import React from 'react';
+import classes from './Work.module.css';
+import Contacts from './Contact/Contacts'
+
 
 const Contact = (props) => {
     return (
         <Fragment>
-            <div>
-                hello
+            <div className={classes.contact}>
+            <Contacts/>
             </div>
         </Fragment>
     )
---
+++ src/Pages/Contact/Contacts.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import React from 'react';
+import classes from './Contact.module.css'
+
+
+const Contacts = (props) => {
+    return (
+        <Fragment>
+            <div className={classes.contact} >
+                <h4>Helll</h4>
+
+            </div>
+        </Fragment>
+    )
+}
+export default Contacts;
