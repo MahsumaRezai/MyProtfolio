ab8381ed8c6333943c384d3b099b411ceb683cdd
Masoumeh Rezai <you@example.com>
2023-09-21 22:01:43 +0430

    add another file

---
+++ src/Pages/Works/UP12/ErrorModalThree.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Card from './Card';
+import classes from './ErrorModalThree.module.css';
+import { Fragment } from 'react';
+import img from '../image/food.png'
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onconfirm} />
+}
+const Modaloerlay = (props) => {
+    return (
+        <Card className={classes.modal}>
+            <div className={classes.head}>
+                <h1>Project information</h1>
+            </div>
+            <div className={classes.content}>
+                <div className={classes.images}>
+                    <img src={img} alt="" className={classes.img} />
+                    <p className={classes.text}>Project Name: Food </p>
+                    <a className={classes.link} href="https://github.com/MahsumaRezai/React_Projec">Github Link: https://github.com/MahsumaRezai/React_Project</a>
+
+                </div>
+            </div>
+        </Card>
+    )
+}
+const ErroModal = (props) => {
+    return (
+        <Fragment>
+            {ReactDom.createPortal(<Backdrop onconfirm={props.onconfirm} />, document.getElementById('backdrop-root'))}
+            {ReactDom.createPortal(<Modaloerlay title={props.title} massage={props.massage} onconfirm={props.onconfirm} />,
+                document.getElementById('overlay-root'))}
+        </Fragment>
+    )
+}
+
+export default ErroModal;
