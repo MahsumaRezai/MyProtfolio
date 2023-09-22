7e409f8d26baa07c19c93846e11fcfe3b0f0c2d1
Masoumeh Rezai <you@example.com>
2023-09-23 02:25:10 +0430

    add last commit

--- src/Componets/Head/Head.js
+++ src/Componets/Head/Head.js
@@ -9,6 +9,7 @@ const Head = (props) => {
     return (
         <Fragment>
             <div className={classes.head}>
+
                 <div className={classes.lists} >
                     <ul className={classes.list}>
                         <li><Link to='/' className={classes.link}>HOME</Link></li>
@@ -19,7 +20,15 @@ const Head = (props) => {
                     <div className={classes.text}>
                         <h1>My protfolio</h1>
                     </div>
-
+                    <div className={classes.textnew}>
+                        <h1>My protfolio</h1>
+                    </div>
+                    <ul className={classes.listes}>
+                        <li><Link to='/' className={classes.links}>HOME</Link></li>
+                        <li><Link to='/ABOUT' className={classes.links}>ABOUT</Link></li>
+                        <li><Link to='/WORK' className={classes.links}>WORKS</Link></li>
+                        <li><Link to='/CONTACT' className={classes.links}>CONTACT</Link></li>
+                    </ul>
                 </div>
                 <Content />
 
--- src/Pages/About/Text.js
+++ src/Pages/About/Text.js
@@ -8,7 +8,7 @@ const Text = (props) => {
             <div className={classes.text}>
                 <h1 className={classes.title}>About Me</h1>
                 <p className={classes.para}>Hello, my name is Masoumeh Rezai. I live in Herat, Afghanistan. I like <br></br>coding I have been
-                   learning programming for 3 year.
+                   working programming for 3 year.
                      During this <br></br> time,
                      I have worked on many projects in this sector and every day I try<br></br> to increase my abilities in this sector.</p>
             </div>
